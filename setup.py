b1a0b8930cb61695eb951a147e9a2e09885236a0
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2018-02-04 20:01:36 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ scripts = [
 desc = 'A fast thermodynamics-based program for checking PCR primer specificity (linux-specific)'
 setup(
     name = 'MFEprimer_linux',
-    version = '2.1',
+    version = '2.1.1',
     description = desc,
     long_description = desc + '\n See README for more information.',
     author = 'Nick Youngblut',
