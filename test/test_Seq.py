5175768a0d5f8b7793a5130da327cd0110ef5b58
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2018-02-11 14:40:23 +0100

    updated test_seq

--- test/test_Seq.py
+++ test/test_Seq.py
@@ -16,28 +16,34 @@ class Test_complement(object):
     def test_simple(self):
         x = 'ATGC'
         y = Seq.complement(x)
+        print('Result: {}'.format(y))
         assert y == 'TACG'
     def test_gap(self):
         x = 'AT-GC'
         y = Seq.complement(x)
+        print('Result: {}'.format(y))
         assert y == 'TA-CG'
         
 class Test_reverse(object):
     def test_simple(self):
         x = 'ATGC'
         y = Seq.reverse(x)
+        print('Result: {}'.format(y))
         assert y == 'CGTA'
     def test_gap(self):
         x = 'AT-GC'
         y = Seq.reverse(x)
+        print('Result: {}'.format(y))
         assert y == 'CG-TA'
 
 class Test_rev_com(object):
     def test_simple(self):
         x = 'ATGC'
         y = Seq.rev_com(x)
+        print('Result: {}'.format(y))
         assert y == 'GCAT'
     def test_gap(self):
         x = 'AT-GC'
         y = Seq.rev_com(x)
+        print('Result: {}'.format(y))
         assert y == 'GC-AT'
